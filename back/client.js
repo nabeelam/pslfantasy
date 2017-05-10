794b89d719d35f6348f8feaee40dde95afdf0c0a
Nabeel Ahmed <nabeelahmed13@gmail.com>
2017-05-10 13:55:54 +0500

    Simpler Structure

--- back/client.js
+++
@@ -1,9 +0,0 @@
-$(() => {
-	const socketio = io()
-	socketio.on('to_client', data => $('#chatlog').append('<hr/>' + data))
-	socketio.on('redirect', function(destination) {
-	    window.location.href = destination;
-	});
-	$('#sendit').click(() => socketio.emit('to_server', $('#username').val() + ',' + $('#password').val()))
-})
-
